ca8438008610318d9d1f43352a88337acca5eff3
Rewas Safwat <62558986+rewas20@users.noreply.github.com>
2023-10-17 18:20:39 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,6 @@ Route::resource('users', UserController::class);
 
 Auth::routes([
     'verify' => true,
-
+]);
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
