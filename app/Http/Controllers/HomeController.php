e77b0b627b11d739deef6ffdb18c0f58c7cf4014
Rewas Safwat <rewasssafwat51@gmail.com>
2023-10-17 13:12:15 +0300

    add verification email and forget password

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,7 +13,8 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        //check verification email with middleware verified or not 
+        $this->middleware(['auth','verified']);
     }
 
     /**
